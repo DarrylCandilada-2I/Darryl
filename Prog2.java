73f910a2e3d9c53ce0aade269bb604a0e9454485
Candilada-2I <dccandilada@paterostechnologicalcollege.edu.ph>
2025-03-14 14:07:44 +0800

    Added Prog2.java

---
+++ Prog2.java
@@ -0,0 +1,9 @@
+public class Prog2
+  public static void main(String[] args) {
+
+    System.out.println("PATEROSTECHNOLOGICALCOLLEGE");
+    System.out.println("DARRYL CANDILADA");
+    System.out.println("DARRYL CANDILADA");
+
+  }
+}
